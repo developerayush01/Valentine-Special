907653dccfe2ec6431a288898be34e473bfea401
Jakie Khan <khanjakie651@gmail.com>
2025-05-13 11:07:31 +0545

    Commit

---
+++ petals.js
@@ -0,0 +1,19 @@
+var sakura = new Sakura('body', {
+        fallSpeed: 0.7,
+        delay: 20,
+        maxSize: 20,
+        minSize: 10,
+        colors: [
+          {
+            gradientColorStart: 'rgba(255, 183, 197, 0.9)',
+            gradientColorEnd: 'rgba(255, 197, 208, 0.9)',
+            gradientColorDegree: 120
+          },
+          {
+            gradientColorStart: 'rgba(255, 102, 128, 0.9)',
+            gradientColorEnd: 'rgba(255, 0, 102, 0.8)',
+            gradientColorDegree: 120
+          },
+        ],
+        delay: 200,
+      });
