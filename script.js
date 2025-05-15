07164fd4505b4d9e9da009d53206f36a6a63d2e5
Jakie Khan <khanjakie651@gmail.com>
2025-05-15 11:00:47 +0545

    New

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-// if(window.innerWidth <=425)
-// {
-//     document.querySelector(".para").style.display="none";
-//     document.querySelector(".para1").style.display="block";
-//     document.querySelector(".para2").style.display="block";
-// }
-// else
-// {
-//     document.querySelector(".para").style.display="block";
-//     document.querySelector(".para1").style.display="none";
-//     document.querySelector(".para2").style.display="none";
-// }
+//  document.addEventListener("contextmenu", (e) => {
+//     e.preventDefault();
+//   });
+
+//   // Disable only Inspect Shortcuts (Not Button Keys)
+//   document.addEventListener("keydown", (e) => {
+//     // Block F12, Ctrl+Shift+I, Ctrl+U
+//     if (
+//       e.key === "F12" ||
+//       (e.ctrlKey && e.shiftKey && e.key === "I") ||
+//       (e.ctrlKey && e.key === "u")
+//     ) {
+//       e.preventDefault();
+//     }
+//   });
+if(window.innerWidth <=425)
+{
+    document.querySelector(".para").style.display="none";
+    document.querySelector(".para1").style.display="block";
+    document.querySelector(".para2").style.display="block";
+}
+else
+{
+    document.querySelector(".para").style.display="block";
+    document.querySelector(".para1").style.display="none";
+    document.querySelector(".para2").style.display="none";
+}
 
 // //     document.addEventListener("DOMContentLoaded", function () {
 // //     const audio = new Audio("audio/Miss U - Everet Almond.mp3");
@@ -22,6 +37,14 @@
 
 let no=document.querySelector(".nobtn")
 no.addEventListener("mouseover",()=>{
+let top = Math.floor(Math.random() * (window.innerHeight - no.offsetHeight));
+    let left = Math.floor(Math.random() * (window.innerWidth - no.offsetWidth));
+no.style.position="absolute";
+no.style.top=`${top}px`;
+no.style.left=`${left}px`;
+})
+
+no.addEventListener("click",()=>{
 let top = Math.floor(Math.random() * (window.innerHeight - no.offsetHeight));
     let left = Math.floor(Math.random() * (window.innerWidth - no.offsetWidth));
 no.style.position="absolute";
